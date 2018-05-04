110a013098a6fb09ea3d354b41db16491e62d8ff
Ido Kleinman <idokleinman@gmail.com>
2018-05-04 16:08:46 -0700

    pipenv run, remove dotenv

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
-from dotenv import load_dotenv
 import os
-from pathlib import Path  # python3 only
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
 
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
